b59296fa8620da886747823a7ed83ff17bbaa1ac
Igor Lapin <ilapin@parallels.com>
2015-12-15 13:19:44 +0300

    Add hardcoded list and detail screen

---
+++ app/src/main/java/com/example/androidtutorialmasterdetailbasic/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidtutorialmasterdetailbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail);
+	}
+}
